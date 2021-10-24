5c35dac3030854bdc717d605a92f9b60027481d0
Gokul Ramakrishnan <ramakrishnan.go@husky.neu.edu>
2021-10-24 02:17:24 -0700

    More React

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Navbar from './components/layout/Navbar';
-// import UserItem from './components/users/UserItem';
 import Users from './components/users/Users';
+import Search from './components/users/Search';
 import './App.css';
 
 class App extends React.Component {
@@ -26,11 +26,28 @@ class App extends React.Component {
 		});
 	};
 
+	searchUsers = async (text) => {
+		this.setState({
+			loading: true
+		});
+		const response = await fetch(
+			`https://api.github.com/search/users?q=${encodeURIComponent(text)}&client_id=${process.env
+				.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+		);
+		const data = await response.json();
+
+		this.setState({
+			loading: false,
+			users: data.items
+		});
+	};
+
 	render() {
 		return (
 			<div className='App'>
 				<Navbar />
 				<div className='container'>
+					<Search searchUsers={this.searchUsers} />
 					<Users users={this.state.users} loading={this.state.loading} />
 				</div>
 			</div>
